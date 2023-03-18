5df99e77bec7a0704b862e934fb0bef27e347b8e
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-18 20:30:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 
 function TodoList() {
-  // Use state to store the list of items
   const [items, setItems] = useState([]);
-
-  // Use state to store the input value
   const [inputValue, setInputValue] = useState('');
 
-  // Function to handle adding an item to the list
   const handleAddItem = () => {
     if (inputValue !== '') {
       setItems([...items, inputValue]);
@@ -15,8 +11,7 @@ function TodoList() {
     }
   };
 
-  // Function to handle removing an item from the list
-  const handleRemoveItem = (index) => {
+    const handleRemoveItem = (index) => {
     const newItems = [...items];
     newItems.splice(index, 1);
     setItems(newItems);
@@ -25,7 +20,7 @@ function TodoList() {
   return (
     <div>
       <h1>To-Do List</h1>
-      {/* Input field to add new items */}
+      
       <input
         type="text"
         placeholder="Enter a task"
@@ -33,7 +28,7 @@ function TodoList() {
         onChange={(event) => setInputValue(event.target.value)}
       />
       <button onClick={handleAddItem}>Add</button>
-      {/* Display the list of items */}
+      
       <ul>
         {items.map((item, index) => (
           <li key={index}>
